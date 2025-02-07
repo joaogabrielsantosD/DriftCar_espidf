43f71dbdfe5fdb8a0073686c572772040373cdad
joaogabrielsantosD <joao.donato@ufpe.br>
2025-02-07 00:42:32 -0300

    Make the library for MX1508 driver

--- components/MX1508/MX1508.c
+++ components/MX1508/MX1508.c
@@ -1,73 +1,131 @@
 #include "MX1508.h"
 
+static const char *TAG = "MX1508";
+
+#define MX1508_CHECK(a, str, ret_val)                             \
+    if (!(a))                                                     \
+    {                                                             \
+        ESP_LOGE(TAG, "%s(%d): %s", __FUNCTION__, __LINE__, str); \
+        return (ret_val);                                         \
+    }
+// #define MOTOR_FREQ_MIN 1000  // 1khz
+// #define MOTOR_FREQ_MAX 20000 // 20kHz
 PWM_values_t values;
 
-void MX1508_init(gpio_config_t *outputpin, ledc_timer_config_t *timer_config,  \
-                ledc_channel_config_t *ch1, ledc_channel_config_t *ch2)
+esp_err_t MX1508_init(const MX1508_config_t *config)
 {
-    ESP_ERROR_CHECK(gpio_config(outputpin));
+    esp_err_t ret;
+    MX1508_CHECK(NULL != config, "Pointer of config is invalid", ESP_ERR_INVALID_ARG);   
 
-    ESP_ERROR_CHECK(ledc_timer_config(timer_config));
+    MX1508_CHECK(config->channel_number > 0 && config->channel_number <= MX1508_UNIT,
+    "Servo channel number out the range", ESP_ERR_INVALID_ARG);
 
-    ESP_ERROR_CHECK(ledc_channel_config(ch1));
+    // MX1508_CHECK(config->freq <= MOTOR_FREQ_MAX && config->freq >= MOTOR_FREQ_MIN,
+    // "Servo pwm frequency out the range", ESP_ERR_INVALID_ARG);
 
-    ESP_ERROR_CHECK(ledc_channel_config(ch2));
-
-    values._CH1 = ch1->channel;
-    values._CH2 = ch2->channel;
-
-    switch (timer_config->duty_resolution)
+    uint64_t pin_mask = 0;
+    uint32_t ch_mask = 0;
+    for (size_t i = 0; i < config->channel_number; i++)
     {
-        case LEDC_TIMER_16_BIT:
-            values._maxpwm = 65535;
-            break;
-
-        case LEDC_TIMER_12_BIT:
-            values._maxpwm = 4095;
-            break;
-
-        case LEDC_TIMER_8_BIT:
-        default:
-            values._maxpwm = 255;
-            break;
+        uint64_t _pin_mask = 1ULL << config->channels.motor_pin[i];
+        uint32_t _ch_mask = 1UL << config->channels.ch[i];
+        MX1508_CHECK(!(pin_mask & _pin_mask), "servo gpio has a duplicate", ESP_ERR_INVALID_ARG);
+        MX1508_CHECK(!(ch_mask & _ch_mask), "servo channel has a duplicate", ESP_ERR_INVALID_ARG);
+        MX1508_CHECK(GPIO_IS_VALID_OUTPUT_GPIO(config->channels.motor_pin[i]), "servo gpio invalid", ESP_ERR_INVALID_ARG);
+        pin_mask |= _pin_mask;
+        ch_mask |= _ch_mask;
     }
+
+    ledc_timer_config_t ledc_timer = {
+        .clk_cfg         = LEDC_AUTO_CLK,
+        .duty_resolution = config->resolution,   // resolution of PWM duty
+        .freq_hz         = config->freq,         // frequency of PWM signal
+        .speed_mode      = SPEED_MODE,           // timer mode
+        .timer_num       = config->timer_number  // timer index
+    };
+
+    ret = ledc_timer_config(&ledc_timer);
+    MX1508_CHECK(ESP_OK == ret, "ledc timer configuration failed", ESP_FAIL);
+    
+    for (size_t i = 0; i < config->channel_number; i++)
+    {
+        ledc_channel_config_t ledc_ch = {
+            .intr_type  = LEDC_INTR_DISABLE,
+            .channel    = config->channels.ch[i],
+            .duty       = 0,
+            .gpio_num   = config->channels.motor_pin[i],
+            .speed_mode = SPEED_MODE,
+            .timer_sel  = config->timer_number,
+            .hpoint     = 0
+        };
+        ret = ledc_channel_config(&ledc_ch);
+        MX1508_CHECK(ESP_OK == ret, "ledc channel configuration failed", ESP_FAIL);
+    }
+
+    values._CH1    = config->channels.ch[0];
+    values._CH2    = config->channels.ch[1];
+    values._maxpwm = (1 << config->resolution) - 1;
+        
+    return ESP_OK;
 }
 
-void motorGo(long pwmSpeed) 
+esp_err_t MX1508_deinit(const MX1508_config_t *config)
 {
-    values._pwmVal = pwmSpeed;
-
-    ledc_set_duty(LEDC_LOW_SPEED_MODE, values._CH1, values._pwmVal);
-    ledc_set_duty(LEDC_LOW_SPEED_MODE, values._CH2, 0);
-
-    ledc_update_duty(LEDC_LOW_SPEED_MODE, values._CH1);
-    ledc_update_duty(LEDC_LOW_SPEED_MODE, values._CH2);
+    for (size_t i = 0; i < config->channel_number; i++)
+        ledc_stop(SPEED_MODE, config->channels.ch[i], 0);
+    ledc_timer_rst(SPEED_MODE, config->timer_number);
+    values._maxpwm = 0;
+    return ESP_OK;
 }
 
-void motorRev(long pwmSpeed) 
+esp_err_t MX1508_read_duty(uint8_t channel, uint32_t *duty)
 {
+    MX1508_CHECK(channel < LEDC_CHANNEL_MAX, "LEDC channel number too large", ESP_ERR_INVALID_ARG);
+    *duty = ledc_get_duty(SPEED_MODE, channel);
+    return ESP_OK;
+}
+
+esp_err_t motorGo(long pwmSpeed) 
+{
+    MX1508_CHECK(pwmSpeed < values._maxpwm, "SPEED is bigger than resolution", ESP_ERR_INVALID_ARG);
     values._pwmVal = pwmSpeed;
 
-    ledc_set_duty(LEDC_LOW_SPEED_MODE, values._CH1, 0);
-    ledc_set_duty(LEDC_LOW_SPEED_MODE, values._CH2, values._pwmVal);
+    ledc_set_duty(SPEED_MODE, values._CH1, values._pwmVal);
+    ledc_set_duty(SPEED_MODE, values._CH2, 0);
 
-    ledc_update_duty(LEDC_LOW_SPEED_MODE, values._CH1);
-    ledc_update_duty(LEDC_LOW_SPEED_MODE, values._CH2);
+    ledc_update_duty(SPEED_MODE, values._CH1);
+    ledc_update_duty(SPEED_MODE, values._CH2);
+
+    return ESP_OK;
+}
+
+esp_err_t motorRev(long pwmSpeed) 
+{
+    MX1508_CHECK(pwmSpeed < values._maxpwm, "SPEED is bigger than resolution", ESP_ERR_INVALID_ARG);
+    values._pwmVal = pwmSpeed;
+
+    ledc_set_duty(SPEED_MODE, values._CH1, 0);
+    ledc_set_duty(SPEED_MODE, values._CH2, values._pwmVal);
+
+    ledc_update_duty(SPEED_MODE, values._CH1);
+    ledc_update_duty(SPEED_MODE, values._CH2);
+
+    return ESP_OK;
 }
 
 void motorBrake() 
 {
-    ledc_set_duty(LEDC_LOW_SPEED_MODE, values._CH1, values._maxpwm);
-    ledc_set_duty(LEDC_LOW_SPEED_MODE, values._CH2, values._maxpwm);
+    ledc_set_duty(SPEED_MODE, values._CH1, values._maxpwm);
+    ledc_set_duty(SPEED_MODE, values._CH2, values._maxpwm);
 
-    ledc_update_duty(LEDC_LOW_SPEED_MODE, values._CH1);
-    ledc_update_duty(LEDC_LOW_SPEED_MODE, values._CH2);
+    ledc_update_duty(SPEED_MODE, values._CH1);
+    ledc_update_duty(SPEED_MODE, values._CH2);
 }
 
 void motorStop()  { stopMotor(); }
 
 void stopMotor() 
-{                      // Kept for backwards compatibility
-    ledc_stop(LEDC_LOW_SPEED_MODE, values._CH1, 0);
-    ledc_stop(LEDC_LOW_SPEED_MODE, values._CH2, 0);
+{   // Kept for backwards compatibility
+    ledc_stop(SPEED_MODE, values._CH1, 0);
+    ledc_stop(SPEED_MODE, values._CH2, 0);
 }
--- components/MX1508/include/MX1508.h
+++ components/MX1508/include/MX1508.h
@@ -3,19 +3,39 @@
 
 #include "driver/ledc.h"
 #include "driver/gpio.h"
-#include "esp_mac.h"
 #include "esp_log.h"
 #include "esp_err.h"
 
+#define MX1508_UNIT 2
+#define SPEED_MODE LEDC_LOW_SPEED_MODE
+
+/**
+ * @brief Configuration of DC motor channel
+ * 
+ */
 typedef struct
 {
-  gpio_num_t pinIN1, pinIN2;     // Pin to MX1508
-  ledc_channel_t ledCH1, ledCH2; // ESP32 ledc Channel for PWM  
-  ledc_timer_bit_t resolution;   // PWM Resolution
-  uint32_t frequecy;             // PWM Freq
+  gpio_num_t motor_pin[MX1508_UNIT]; /**< Pin number of pwm output */
+  ledc_channel_t ch[MX1508_UNIT];    /**< The ledc channel which used */
+} MX1508_channel_t;
 
+/**
+ * @brief Configuration of DC motor
+ * 
+ */
+typedef struct
+{
+  uint32_t freq;             /**< PWM frequency */
+  ledc_timer_t timer_number; /**< Timer number of ledc */
+  ledc_timer_t resolution;   /**< Channel resolution */
+  MX1508_channel_t channels; /**< Channels to use */
+  uint8_t channel_number: MX1508_UNIT; /**< Total channel number */
 } MX1508_config_t;
 
+/**
+ * @brief Auxiliar struct for save the PWM values and Pins/Channel
+ * 
+ */
 typedef struct
 {
   ledc_channel_t _CH1, _CH2;
@@ -24,13 +44,72 @@ typedef struct
   long _maxpwm;                  // Max PWM Value of the Motor
 } PWM_values_t;
 
-void MX1508_init(gpio_config_t *outputpin, ledc_timer_config_t *timer_config,  \
-                ledc_channel_config_t *ch1, ledc_channel_config_t *ch2);
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-void motorGo(long pwmVal); 
-void motorRev(long pwmVal);
+/**
+ * @brief Initialize ledc to control the Motor
+ *
+ * @param config Pointer of Motor configure struct
+ *
+ * @return
+ *     - ESP_OK Success
+ *     - ESP_ERR_INVALID_ARG Parameter error
+ *     - ESP_FAIL Configure ledc failed
+ */
+esp_err_t MX1508_init(const MX1508_config_t *config);
+
+/**
+ * @brief Deinitialize ledc for motor
+ *
+ * @param config Pointer of Motor configure struct
+ *
+ * @return
+ *     - ESP_OK Success
+ */
+esp_err_t MX1508_deinit(const MX1508_config_t *config);
+
+/**
+ * @brief Read current duty of one channel
+ *
+ * @param channel LEDC channel, select from ledc_channel_t
+ * @param duty Current duty of the channel
+ *
+ * @return
+ *     - ESP_OK Success
+ *     - ESP_ERR_INVALID_ARG Parameter error
+ */
+esp_err_t MX1508_read_duty(uint8_t channel, uint32_t *duty);
+
+/**
+ * @brief Set the motor to a certain spee
+ *
+ * @attention motorGo is foward and motorRev is reverse
+ * 
+ * @param channel LEDC channel, select from ledc_channel_t
+ * @param duty Current duty of the channel
+ *
+ * @return
+ *     - ESP_OK Success
+ *     - ESP_ERR_INVALID_ARG Parameter error
+ */
+esp_err_t motorGo(long pwmVal); 
+esp_err_t motorRev(long pwmVal); // Reverse
+
+/**
+ * @brief Hard stop
+ */
 void motorBrake(void);                // Sets boths signals to high to hard brake the motor 
+
+/**
+ * @brief Soft stop
+ */
 void motorStop(void);                 // Sets both signals to low to allow motor to spin down
 void stopMotor(void);                 // Kept for backwards compatibility
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
