617c4770fd40b1edeb640662503c1a6126d49c97
joaogabrielsantosD <joao.donato@ufpe.br>
2025-02-12 00:25:01 -0300

    Add esp-now definitions

--- main/include/driver_defs.h
+++ main/include/driver_defs.h
@@ -1,6 +1,7 @@
 #ifndef LEDC_DEFS_H
 #define LEDC_DEFS_H
 
+#include "sdkconfig.h"
 #include "driver/gpio.h"
 #include "driver/ledc.h"
 #include "driver/adc.h"
@@ -8,10 +9,19 @@
 #include "esp_chip_info.h"
 #include "esp_flash.h"
 #include "esp_system.h"
+#include "esp_timer.h"
 #include "math.h"
+// EEPROM storage
+#include "nvs_flash.h"
+#include "nvs.h"
+// ESP-NOW
+#include "esp_netif.h"
+#include "esp_wifi.h"
+#include "esp_mac.h"
+#include "esp_now.h"
 
 #define println() (printf("\r\n"))
-#define delay(ms) (vTaskDelay(ms / portTICK_PERIOD_MS))
+#define delay(ms) (vTaskDelay(pdMS_TO_TICKS(ms)))
 
 /* GPIO defines */
 #define GPIO_MODE        GPIO_MODE_INPUT_OUTPUT
@@ -59,5 +69,21 @@
 #define SOC_ADC_CHANNEL  ADC1_CHANNEL_6
 #define SOC_WIDTH        ADC_WIDTH_BIT_12
 #define SOC_ATTENUATION  ADC_ATTEN_DB_12
+
+/* ESP-NOW Definitions */
+#if CONFIG_ESPNOW_WIFI_MODE_STATION
+    #define ESPNOW_WIFI_MODE WIFI_MODE_STA
+    #define ESPNOW_WIFI_IF   ESP_IF_WIFI_STA
+#else
+    #define ESPNOW_WIFI_MODE WIFI_MODE_AP
+    #define ESPNOW_WIFI_IF   ESP_IF_WIFI_AP
+#endif
+
+#if CONFIG_ESPNOW_ENABLE_POWER_SAVE
+    #define CONFIG_ESPNOW_WAKE_WINDOW   1000
+    #define CONFIG_ESPNOW_WAKE_INTERVAL 1000
+#endif  
+
+#define CONFIG_ESPNOW_CHANNEL 1
 
 #endif
